c1cf78e9dcafa98d550fd4b7d07310b8db2f6d2f
Girish <girishpandey183@gmail.com>
2025-11-28 16:09:26 +0530

    initial project upload

--- Backend/route/Authrouter.js
+++
@@ -1,9 +0,0 @@
-const { signup, login } = require('../controller/Authcontroller');
-const { signupValidation, loginValidation } = require('../middleware/Authvalidation');
-const router = require('express').Router();
-
-// ✅ Route should look like this
-router.post('/signup', signupValidation, signup);
-router.post('/login', loginValidation, login);
-
-module.exports = router;
